db5b795362c1fc50b3dd086817c457e402b5f768
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-04-06 17:37:46 +0900

    Update test.java

--- workspace/ossspring/src/test/java/com/mycompany/myapp/test.java
+++ workspace/ossspring/src/test/java/com/mycompany/myapp/test.java
@@ -1,9 +1,12 @@
 package com.mycompany.myapp;
 
 public class test {
+<<<<<<< HEAD
 	public static void main (String args[]) {
 		System.out.println("test2");
 		System.out.println("test3");
 	}
+=======
+>>>>>>> parent of f623a6a (branch4 test)
 
 }
