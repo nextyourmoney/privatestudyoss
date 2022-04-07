48c66bb6ad1f76a6ccb0b97af00ed3e5f3b57710
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-04-07 14:57:06 +0900

    restart spring setting with homework

---
+++ workspace/ossspring_homework/src/test/java/com/mycompany/myapp/test.java
@@ -0,0 +1,15 @@
+package com.mycompany.myapp;
+
+public class test {
+
+	public static void main (String args[]) {
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test3");
+		System.out.println("tmer");
+		
+	}
+
+
+
+}
