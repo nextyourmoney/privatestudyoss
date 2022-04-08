12a18d843868a52757808cc3f2922e853668f6a6
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-04-08 09:06:54 +0900

    Aklgorithm0408

---
+++ workspace/JavaAlgorithm/src/Algorithm_0406/level2_targetNumber.java
@@ -0,0 +1,10 @@
+package Algorithm_0406;
+
+public class level2_targetNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ workspace/JavaAlgorithm/src/Algorithm_0407/Silver2_1706.java
@@ -0,0 +1,10 @@
+package Algorithm_0407;
+
+public class Silver2_1706 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
