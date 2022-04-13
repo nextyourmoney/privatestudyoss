64b9270983f07faea93aa743c94eaa95fc38a449
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-04-13 10:28:02 +0900

    0413_Spring_HttpServletRequest

---
+++ workspace/ossspring/src/main/java/com/mycompany/myapp/controller/Ch05Controller.java
@@ -0,0 +1,43 @@
+package com.mycompany.myapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import com.mycompany.myapp.controller.dto.Ch04Member;
+import com.mycompany.myapp.controller.dto.ch04Dto;
+import com.mycompany.myapp.validator.Ch04MemberEmailValidator;
+import com.mycompany.myapp.validator.Ch04MemberJoinFormValidator;
+import com.mycompany.myapp.validator.Ch04MemberLoginFormValidator;
+import com.mycompany.myapp.validator.Ch04MemberIdValidator;
+import com.mycompany.myapp.validator.Ch04MemberPasswordValidator;
+import com.mycompany.myapp.validator.Ch04MemberTelValidator;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/ch05")
+@Log4j2
+public class Ch05Controller {
+	@RequestMapping("/content")
+	public String content() {
+		return "ch05/content";
+	}
+	
+	@GetMapping("/getHeaderValue")
+	public String getHeadervalue(HttpServletRequest requeest) {
+		
+		return "redirect:/ch05/content";
+	}
+	
+		
+		
+	
+	
+}
+
--- workspace/ossspring/src/main/webapp/WEB-INF/views/common/menu.jsp
+++ workspace/ossspring/src/main/webapp/WEB-INF/views/common/menu.jsp
@@ -5,19 +5,28 @@
          <span class="mr-2">Ch01.</span> <a href="${pageContext.request.contextPath}/ch01/content" class="text-white">/ch01/content: 컨트롤러와 뷰 동작 이해</a>
       </h6>
    </li>
+   
    <li class="nav-item mb-2">
       <h6 class="text-white">
          <span class="mr-2">Ch02.</span> <a href="${pageContext.request.contextPath}/ch02/content" class="text-white">Controller/RequestMapping</a>
       </h6>
    </li>
+   
    <li class="nav-item mb-2">
       <h6 class="text-white">
          <span class="mr-2">Ch03.</span> <a href="${pageContext.request.contextPath}/ch03/content" class="text-white">Controller/Request Parameter</a>
       </h6>
    </li>
+   
    <li class="nav-item mb-2">
       <h6 class="text-white">
          <span class="mr-2">Ch04.</span> <a href="${pageContext.request.contextPath}/ch04/content" class="text-white">Controller/validation</a>
+      </h6>
+   </li>
+   
+   <li class="nav-item mb-2">
+      <h6 class="text-white">
+         <span class="mr-2">Ch05.</span> <a href="${pageContext.request.contextPath}/ch05/content" class="text-white">Controller/test</a>
       </h6>
    </li>
 </ul>
