f7b07435f23d282b7b56332a7102327fe7e79a54
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-04-19 09:55:39 +0900

    0419_spring_기본 생성자를 이용한 객체 생성 방법(XML)

---
+++ workspace/ossspring/src/main/java/com/mycompany/myapp/dao/Ch13Dao2CreateByXML.java
@@ -0,0 +1,15 @@
+package com.mycompany.myapp.dao;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Ch13Dao2CreateByXML {
+	public Ch13Dao2CreateByXML() {
+		log.info("tlfgod");
+	}
+	
+	
+	
+}
---
+++ workspace/ossspring/src/main/java/com/mycompany/myapp/service/Ch13Service2CreateByXML.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Ch13Service2CreateByXML {
+	public Ch13Service2CreateByXML() {
+		log.info("tlfgog");
+	}
+
+}
