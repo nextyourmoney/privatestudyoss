fe0f29fc2c95b7e3de8db2721bac1bd8a897b3b7
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-05-24 13:59:30 +0900

    .

--- projects/node-workspace/front-end-vue/front-end-vue/src/router/menu06.js
+++
@@ -1,6 +0,0 @@
-export default [
-    {
-        path: "/menu06/exam01rootstate",
-        component: () => import(/*webpackChunkName: "menu05" */ "../views/menu06/Exam01RootState.vue"),
-    },
-];
--- projects/node-workspace/front-end-vue/front-end-vue/src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="card">
-    <div class="card-header">AboutView</div>
-    <div class="card-body">
-      <div class="about">This is an about page</div>
-    </div>
-  </div>
-</template>
