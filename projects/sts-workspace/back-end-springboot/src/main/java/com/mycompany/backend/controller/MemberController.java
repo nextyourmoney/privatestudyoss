729112d382c75fdadc28c6eb8cf2aa7c72835f6e
nextyourmoney <47136473+nextyourmoney@users.noreply.github.com>
2022-05-30 11:02:09 +0900

    spring 회원 가입 시 인증 처리, 로그인 인증 처리

--- projects/sts-workspace/back-end-springboot/src/main/java/com/mycompany/backend/controller/MemberController.java
+++ projects/sts-workspace/back-end-springboot/src/main/java/com/mycompany/backend/controller/MemberController.java
@@ -3,10 +3,13 @@ package com.mycompany.backend.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.backend.dto.Member;
 import com.mycompany.backend.security.Jwt;
+import com.mycompany.backend.service.MemberService;
+import com.mycompany.backend.service.MemberService.JoinResult;
+import com.mycompany.backend.service.MemberService.LoginResult;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -22,18 +28,60 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/member") //공통 경로
 
 public class MemberController {
+	
+	@Resource 
+	private MemberService memberService;
+	
+	@Resource PasswordEncoder passwordEncoder;
+	
 	@PostMapping("/join") //회원가입
 	public Map<String, Object> join(@RequestBody Member member){ //RequestBody는 JSON Data가 들어가야 한다. 
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("result", "success");
+//		map.put("member", member);
+		
+		//계정 활성황
+		member.setMenabled(true);
+		//패스워드 암호화
+		member.setMpassword(passwordEncoder.encode(member.getMpassword()));
+		//회원가입 처리
+		JoinResult joinResult = memberService.join(member);
+		//응답 생성
 		Map<String, Object> map = new HashMap<>();
-		map.put("result", "success");
-		map.put("member", member);
+		if(joinResult == JoinResult.SUCCESS) {
+			map.put("result", "success");
+		} else if(joinResult == JoinResult.DUPLICATED) {
+			map.put("result", "duplicated");
+		} else {
+			map.put("result", "fail");
+		}
 		return map;
 	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody Member member){
-		String accessToken = Jwt.createAccessToken(member.getMid(), "ROLE_USER"); //보안 토큰 생성
-		String refreshToken = Jwt.createRefreshToken(member.getMid(), "ROLE_USER"); //토큰 재활성?
+		log.info("실행");
+		
+		//로그인 인증 
+		//mid와 mpassword가 없을 경우
+		if(member.getMid() == null || member.getMpassword() == null) { //에러 밑 인증 과정 처리
+			return ResponseEntity
+					.status(401) //클라이언트 인증 오류 발생
+					.body("mid or mpassword cannot be null");
+		}
+		
+		//로그인 결과 얻기
+		LoginResult loginResult = memberService.login(member);
+		
+		if(loginResult != LoginResult.SUCCESS) {
+			return ResponseEntity
+					.status(401) //클라이언트 인증 류 발생
+					.body("mid or mpassword is wrong");
+		}
+		
+		Member dbMember = memberService.getMember(member.getMid());
+		String accessToken = Jwt.createAccessToken(member.getMid(), dbMember.getMrole()); //보안 토큰 생성
+		String refreshToken = Jwt.createRefreshToken(member.getMid(), dbMember.getMrole()); //토큰 재활성?
 		
 		//쿠키 생성
 		String refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
